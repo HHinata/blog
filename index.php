870941d04cd4849f00c06047dc17b208c5eab317
hinata <869628168@qq.com>
2017-06-08 15:43:46 +0800

    add form

--- index.php
+++ index.php
@@ -14,3 +14,28 @@ echo "<br>"."xxxx";
 conn_out($conn);
 echo "hello world\n";
 ?>
+<script language=javascript> 
+function login()
+{ 
+	form.action=”login.php”; 
+	form.submit();
+} 
+function regist()
+{ 
+	form.action=”regist.php”; 
+	form.submit();
+} 
+</script> 
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<body>
+<h2>Hinata</h2>
+<form name="login" action="index.php"  method="post">
+user  id:<input type="text" name="uid"><br>
+password:<input type="password" name="pass"><br>
+<input type="button" value="登录" onclick="login();">
+<input type="button" value="注册" onclick="regist();">
+</form>
+</body>
+</html>
